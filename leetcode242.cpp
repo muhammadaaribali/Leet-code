559eacedb36df2012b19457ccab540c48b79a4c3
muhammadaaribali <muhammadaarib7452@gmail.com>
2026-01-27 23:10:40 +0500

    done w problem 242

---
+++ leetcode242.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<unordered_map>
+#include<string>
+using namespace std;
+
+int main(){
+
+    unordered_map<int,int> map;
+    unordered_map<int,int> map2;
+    string s="anagram";
+    string t="nagaram";
+
+    for(char c:s){
+
+        int key=c-'a';
+        map[key]++;
+    }
+
+    for(char c: t){
+
+        int key=c-'a';
+        map2[key]++;
+    }
+
+    if(map==map2){
+        cout<<"true"<<endl;
+    }
+    else{
+        cout<<"false"<<endl;
+    }
+
+
+    return 0;
+}
